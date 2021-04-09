da805c96d81698b0912948ad22b4a5c1aeb934b3
Vlad F <vlad.faget@outlook.com>
2021-04-09 13:57:19 +0200

    kind of
    works

--- app/src/main/java/com/example/mandatoryassignment/timeline.java
+++ app/src/main/java/com/example/mandatoryassignment/timeline.java
@@ -8,7 +8,14 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class timeline extends AppCompatActivity {
 
@@ -31,6 +38,26 @@ public class timeline extends AppCompatActivity {
     }
     public void getandshowallposts()
     {
+        MessageService mess= ApiUtils.getBookStoreService();
+        Call<List<Message>> getallMessagesCall= mess.getAllMessages();
+        getallMessagesCall.enqueue(new Callback<List<Message>>() {
+            @Override
+            public void onResponse(Call<List<Message>> call, Response<List<Message>> response) {
+                if (response.isSuccessful()) {
+                    List<Message> allMessages= response.body();
+                    Log.d("banana", allMessages.toString());
+                }
+                else {
+                    String message = "Problem " + response.code() + " " + response.message();
+                    Log.d("banana", message);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Message>> call, Throwable t) {
+                Log.e("banana", t.getMessage());
+            }
+        });
 
     }
 }
